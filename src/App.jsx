862307f365b5498db76a8e6e8c6ea68a41d3d9e3
Pratikkk <kenipratik9@gmail.com>
2023-10-15 15:49:55 +0530

    fix styling

--- src/App.jsx
+++ src/App.jsx
@@ -71,7 +71,7 @@ function App() {
         >
           {buttonText}
         </button>
-        <button className='px-4 py-2 bg-cyan-500 text-white shrink-0 my-4 rounded-md hover:bg-cyan-700 hover:scale-150 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300 ease-out duration-300' onClick={generatePassword}>Randomize</button>
+        <button className='px-4 py-2 bg-cyan-800 text-white shrink-0 my-4 rounded-md hover:bg-cyan-700 hover:scale-150 active:bg-cyan-700 focus:outline-none focus:ring focus:ring-cyan-300 ease-out duration-300' onClick={generatePassword}>Randomize</button>
         <div className='flex text-sm gap-x-2'>
           <div className='flex items-center gap-x-1'>
             <input
